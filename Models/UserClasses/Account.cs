74ee607c8cfc9c7465d5a1f3c174459d3eddef19
Cyrill Josh Odato <151166468+CyrillJosh@users.noreply.github.com>
2025-09-25 21:36:49 +0800

    Add files via upload

---
+++ Models/UserClasses/Account.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TheSocialCebu_Capstone.Models;
+
+public partial class Account
+{
+    public string AccountId { get; set; } = null!;
+
+    public string PersonId { get; set; } = null!;
+
+    public string Username { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public virtual Person Person { get; set; } = null!;
+}
